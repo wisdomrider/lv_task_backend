23214736da429743a6a99d1a907f615c1ef69008
wisdomrider <avishekzone@gmail.com>
2024-07-28 13:02:43 +0545

    postgres environment variable added

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     PORT = os.getenv('PORT', 5001)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URL') or \
         'sqlite:///' + os.path.join(basedir, 'calendar.db')
-    if os.environ.get('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    if os.environ.get('DB_URL'):
+        SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     REDIS_URL = os.getenv('REDIS_URL')
     MAIL_SERVER = os.getenv('MAIL_HOST')
